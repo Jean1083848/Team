2f1c602356cfd688665f10d1366743cbe8ff54c6
Jean1083848 <80384176+Jean1083848@users.noreply.github.com>
2021-06-25 21:12:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
